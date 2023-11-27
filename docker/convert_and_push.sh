e9b85945afe64aaf7152c5753224dfc43b7e0851
berdancaglaraydin <berdancaglaraydin@gmail.com>
2023-11-27 11:03:39 +0100

    refactoring

--- docker/convert_and_push.sh
+++ docker/convert_and_push.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-# Exit if any command returns a non-zero status
 set -e
 
 validate_variables() {
-  for var in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY S3_PATH; do
+  required_vars=("AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY")
+
+  for var in "${required_vars[@]}"; do
     if [ -z "${!var}" ]; then
       echo "Error: $var is not set."
       exit 1
     fi
   done
+
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+    echo "Error: Some arguments are missing. (s3_bucket_base_path, s3_path, version, test_report_filename)"
+    exit 1
+  fi
 }
 
-validate_variables
+convert_xml_to_html() {
+  echo "Converting $test_report_filename.xml to HTML..."
+  python -m junit2htmlreport "$test_report_filename.xml" "$test_report_filename.html"
+}
 
-if [ -z "$1" ]; then
-  echo "Error: Version argument is missing."
-  exit 1
-fi
+rename_html_file() {
+  new_filename=$(date +"%Y-%m-%d_%H-%M-%S")
+  echo "Renaming $test_report_filename.html to $new_filename.html..."
+  mv "$test_report_filename.html" "$new_filename.html"
+}
 
-echo "Validation successful. Continuing with the execution."
+copy_to_s3() {
+  s3_bucket_base_path="$1"
+  s3_path="$2"
+  version="$3"
+  s3_full_path="$s3_bucket_base_path/$s3_path/$version/"
+  echo "Copying $new_filename.html to S3 bucket ($s3_full_path)..."
+  aws s3 cp "$new_filename.html" "$s3_full_path"
+}
 
-echo "Converting test_report.xml to HTML..."
-python -m junit2htmlreport test_report.xml test_report.html
+validate_variables "$@"
+echo "Variable validation successful. Continuing with the execution."
 
-new_filename=$(date +"%Y-%m-%d_%H-%M-%S")
-echo "Renaming test_report.html to $new_filename.html..."
-mv test_report.html $new_filename.html
+test_report_filename="$4"
 
-version=$1
+convert_xml_to_html
+rename_html_file
+copy_to_s3
 
-echo "Copying $new_filename.html to S3 bucket (s3://empatica-test-reports-data/$S3_PATH/$version/)..."
-aws s3 cp $new_filename.html "s3://empatica-test-reports-data/$S3_PATH/$version/"
-rm $new_filename.html
-
+rm "$new_filename.html"
 echo "Done."
